dae34730d657cc77cc967eacb2d8b58df6bdff1b
yzned <harlydeaz@gmail.com>
2025-08-07 18:28:53 +0600

    create first section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
+
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import compress from 'astro-compress';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static',
+
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': new URL('./src', import.meta.url).pathname,
+        '@assets': new URL('./src/assets', import.meta.url).pathname,
+        '@components': new URL('./src/components', import.meta.url).pathname,
+      },
+    },
+  },
+
+  integrations: [compress()],
+});
